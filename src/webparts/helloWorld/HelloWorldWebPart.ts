309ab6fafc381698d488fc021b7480dae8587026
Kevin T. Coughlin <KevinTCoughlin@users.noreply.github.com>
2019-11-17 06:55:07 -0800

    yo @microsoft/generator-sharepoint@1.4.1
    ms-Icon test

---
+++ src/webparts/helloWorld/HelloWorldWebPart.ts
@@ -0,0 +1,63 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+import { Version } from '@microsoft/sp-core-library';
+import {
+  BaseClientSideWebPart,
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField
+} from '@microsoft/sp-webpart-base';
+
+import * as strings from 'HelloWorldWebPartStrings';
+import HelloWorld from './components/HelloWorld';
+import { IHelloWorldProps } from './components/IHelloWorldProps';
+
+export interface IHelloWorldWebPartProps {
+  description: string;
+}
+
+export default class HelloWorldWebPart extends BaseClientSideWebPart<IHelloWorldWebPartProps> {
+
+  public render(): void {
+    const element: React.ReactElement<IHelloWorldProps > = React.createElement(
+      HelloWorld,
+      {
+        description: this.properties.description
+      }
+    );
+
+    const iconElement: React.ReactElement<{}> = React.createElement(
+      'i',
+      {
+        className: 'ms-Icon ms-Icon--RecurringEvent'
+      }
+    );
+
+    ReactDom.render(iconElement, this.domElement);
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
